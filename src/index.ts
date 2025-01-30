bf515c8239274e0d1c4082b6ee01ed5212e01a5d
Ashwani Mishra <ashwani01superman@gmail.com>
2025-01-30 22:28:05 +0530

    backend

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,115 @@
 import express from "express";
-import mongoose from "mongoose";
+// import { random } from "./utils";
 import jwt from "jsonwebtoken";
+import { ContentModel, UserModel } from "./db";
+import { JWT_PASSWORD } from "./config";
+import { userMiddleware } from "./middleware";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.post("/api/v1/signup", (req, res) => {
+app.post("/api/v1/signup", async (req, res) => {
+    // TODO: zod validation , hash the password
+    const username = req.body.username;
+    const password = req.body.password;
 
-});
+    try {
+        await UserModel.create({
+            username: username,
+            password: password
+        }) 
 
-app.post("/api/v1/signin", (req, res) => {
+        res.json({
+            message: "User signed up"
+        })
+    } catch(e) {
+        console.log('====================================');
+        console.log(e);
+        console.log('====================================');
+        res.status(411).json({
+        
+            message: "User already exists"
+        })
+    }
+})
 
-});
+app.post("/api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
-app.post("/api/v1/content", (req, res) => {
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if (existingUser) {
+        const token = jwt.sign({
+            id: existingUser._id
+        }, JWT_PASSWORD)
 
-});
+        res.json({
+            token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrrect credentials"
+        })
+    }
+})
 
-app.get("/api/v1/content", (req, res) => {
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+    const link = req.body.link;
+    const type = req.body.type;
+    
+    await ContentModel.create({
+        link,
+        type,
+        title: req.body.title,
+        userId: req.userId,
+        tags: []
+    })
 
-});
+    res.json({
+        message: "Content added"
+    })
+    
+})
 
-app.delete("/api/v1/content", (req, res) => {
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
+    // @ts-ignore
+    const userId = req.userId;
+    const content = await ContentModel.find({
+        userId: userId
+    }).populate("userId", "username")// i want name as well 
+    res.json({
+        content
+    })
+})
 
-});
+app.delete("/api/v1/content", userMiddleware, async (req, res) => {
+    const contentId = req.body.contentId;
 
-app.post("/api/v1/brain/share", (req, res) => {
+    await ContentModel.deleteMany({
+        contentId,
+        userId: req.userId
+    })
 
-});
+    res.json({
+        message: "Deleted"
+    })
+})
 
-app.get("/api/v1/brain/:shareLink", (req, res) => {
+app.post("/api/v1/brain/share", userMiddleware, async (req, res) => {
+    const share = req.body.share;
+    
+})
 
-});
+app.get("/api/v1/brain/:shareLink", async (req, res) => {
+    const hash = req.params.shareLink;
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+  
+
+})
+
+app.listen(3000);
